364d5cd8a87bec276397abb584a15aba6c5b6938
TakumiHendricksDev <takumihendricksdev@gmail.com>
2023-03-01 13:39:06 -0600

    current changes

---
+++ RecipeApp/src/views/CreateView.vue
@@ -0,0 +1,5 @@
+<template>
+    <main>
+        <h1>This is the Create View</h1>
+    </main>
+</template>
---
+++ RecipeApp/src/views/MyRecipesView.vue
@@ -0,0 +1,5 @@
+<template>
+    <main>
+        <h1>This is the My Recipes View</h1>
+    </main>
+</template>
