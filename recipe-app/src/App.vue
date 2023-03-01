8da5fc18725734283f9f5d8eecdb1948716135c6
TakumiHendricksDev <takumihendricksdev@gmail.com>
2023-03-01 14:10:40 -0600

    Updated file with tailwindcss

---
+++ recipe-app/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue';
+import { RouterView } from 'vue-router';
+</script>
+
+<template>
+  <Navbar></Navbar>
+
+  <RouterView></RouterView>
+</template>
---
+++ recipe-app/src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+    <main>
+        <div>
+            <slot name="logo"></slot>
+        </div>
+        <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+        </nav>
+    </main>
+</template>
